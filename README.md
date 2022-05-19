ff85394aa59a1c57b652f6facc41f52357407163
chiragduhan <86667939+chiragduhan@users.noreply.github.com>
2022-05-19 15:56:15 +0530

    update

--- README.md
+++
@@ -1,3 +0,0 @@
-# its a website :D
-<li><a href="http://binarystar.me/">Website</a><ul></ul>
-
--- script.js
+++
@@ -1,7 +0,0 @@
-// by chirag
-console.log("By Chirag Duhan");
-
-document.getElementById("bt").onclick = function() {
-innerHTML = Date()
-}
-
